c1c4db714852cabb2768802dd9bc93ff68e82660
Dillon <smedleydillon@gmail.com>
2020-02-26 21:49:04 -0500

    Finalize deploy pipeline

--- chirpreport/__init__.py
+++ chirpreport/__init__.py
@@ -28,7 +28,7 @@ def home():
     return '''<h1>Chirp Report API</h1>
 <p>This is a flask api for sentiment analysis
 <br/>
-Powered by Chirp Report</p>'''
+Powered by Chirp Report test</p>'''
 
 
 @app.route('/api/v1/resources/books/all', methods=['GET'])
