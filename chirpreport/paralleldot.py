d3397cdf133a6b294107027832a42839c52bdf60
Youmin Zheng <zheng.youmin@gmail.com>
2020-03-03 22:50:55 -0500

    Changed the function to create a new tweets_cleaned file and tweets_new file

--- chirpreport/paralleldot.py
+++ chirpreport/paralleldot.py
@@ -3,21 +3,21 @@ import paralleldots
 import pandas as pd
 import re
 
-
+# Cleans the text in a array of ['id', 'text']
 def clean_tweets(tweet):
-    tweet = re.sub(r'$\w', '', tweet)  # remove stock market tickers like $GE
-    tweet = re.sub(r'@\w', '', tweet)  # remove stock market tickers like $GE
-    tweet = re.sub(r'^RT[\s]+', '', tweet)  # remove old style retweet text "RT"
-    tweet = re.sub(r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', '',
-                   tweet)  # remove hyperlinks
-    tweet = re.sub(r'#', '', tweet)  # remove hashtags only removing the hash # sign from the word
+    tweet[1] = re.sub(r'$\w', '', tweet[1])  # remove stock market tickers like $GE
+    tweet[1] = re.sub(r'@\w', '', tweet[1])  # remove stock market tickers like $GE
+    tweet[1] = re.sub(r'^RT[\s]+', '', tweet[1])  # remove old style retweet text "RT"
+    tweet[1] = re.sub(r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', '',
+                   tweet[1])  # remove hyperlinks
+    tweet[1] = re.sub(r'#', '', tweet[1])  # remove hashtags only removing the hash # sign from the word
     return tweet
 
-
+# Cleans the initial tweet csv and removes all the unused columns
 def create_clean_tweets():
-    tweets = pd.read_csv("tweets.csv").values
-    cleaned = [clean_tweets(tweet[0]) for tweet in tweets]
-    df = pd.DataFrame(cleaned, columns=['text'])
+    tweets = pd.read_csv("tweets.20150430-223406.tweet.csv", usecols=['id', 'text']).values
+    cleaned = [clean_tweets(tweet) for tweet in tweets]
+    df = pd.DataFrame(cleaned, columns=['id', 'text'])
     df.to_csv("tweets_cleaned.csv", index=False)
 
 
@@ -45,15 +45,24 @@ def analyze(input_file, output_file):
     df.to_csv(input_file, index=False)
 
 
-# create_clean_tweets()
+create_clean_tweets()
 
 
+def add_id_to_already_classified_tweets():
+    # This function is not finished
+    tweetsWithID = pd.read_csv('tweets_cleaned.csv').values
+    classifiedTweetsWithoutID = pd.read_csv('classified_tweets.csv', encoding='iso-8859-1').values
 
-for i in range(20):
-    for key in keys:
-        print(key)
-        paralleldots.set_api_key(key)
-        # paralleldots.sentiment( text )
-        analyze('tweets_cleaned.csv', 'classified_tweets.csv')
+    return 1
 
-response = "{'emotion': [{'Excited': 0.0947050187, 'Happy': 0.09413058, 'Bored': 0.0944737127, 'Sad': 0.1304103751, 'Fear': 0.1717756152, 'Angry': 0.4145046983}, {'Excited': 0.1811228963, 'Happy': 0.102223794, 'Bored': 0.1367001269, 'Sad': 0.1669378585, 'Fear': 0.3188618448, 'Angry': 0.0941534795}]}"
+
+add_id_to_already_classified_tweets();
+
+# for i in range(20):
+#     for key in keys:
+#         print(key)
+#         paralleldots.set_api_key(key)
+#         # paralleldots.sentiment( text )
+#         analyze('tweets_cleaned.csv', 'classified_tweets.csv')
+
+# response = "{'emotion': [{'Excited': 0.0947050187, 'Happy': 0.09413058, 'Bored': 0.0944737127, 'Sad': 0.1304103751, 'Fear': 0.1717756152, 'Angry': 0.4145046983}, {'Excited': 0.1811228963, 'Happy': 0.102223794, 'Bored': 0.1367001269, 'Sad': 0.1669378585, 'Fear': 0.3188618448, 'Angry': 0.0941534795}]}"
